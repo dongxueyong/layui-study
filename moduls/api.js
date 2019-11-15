c572717ebe4d90b604bba8b3c628a9f1e24d6131
dong <3248838607@qq.om>
2019-11-15 11:08:33 +0800

    layui

---
+++ moduls/api.js
@@ -0,0 +1,8 @@
+layui.define(["layer"], function(exports) {
+	var obj = {
+		login : function(){
+			console.log(555);
+		}
+	}
+	exports('api', obj);
+});
---
+++ moduls/demo.js
@@ -0,0 +1,11 @@
+//layui模块的定义
+layui.define(["layer"], function(exports) {
+	var obj = {
+		login : function(){
+			console.log(111)
+		}
+	}
+	exports('demo', obj);
+});
+
+
